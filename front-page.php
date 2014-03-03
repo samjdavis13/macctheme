9511d89a019dd835f6855fd97f3d932bb3f94c62
Sam <samjd1993@gmail.com>
2014-03-03 12:38:46 +1100

    add static header

--- front-page.php
+++ front-page.php
@@ -9,6 +9,14 @@
 		$the_query = new WP_Query( $args );
 	?>
 
+	<div class='fullspan feature'>
+		<div class='container_12 slider'>
+			<ul>
+		        <li><img src="<?php bloginfo('template_directory'); ?>/img/logo.png" id='mainlogo'></li>
+		    </ul>
+		</div>
+	</div>
+
 	<?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		<div class='fullspan article'>
 			<article class='container_12'>
