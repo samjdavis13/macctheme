f22cb74c6c67d175a0c55ade44816d202d84b984
Sam <samjd1993@gmail.com>
2014-03-03 16:01:10 +1100

    Bring index and page up to scratch

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
-<p>This is the index.php file</p>
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
-
-<?php endwhile; else:?>
-
-	<p>There are no posts or pages here</p>
-
-<?php endif; ?>
+	<div id='articlewrapper'>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class='fullspan article'>
+				<article class='container_12'>
+					<h1><?php the_title(); ?></h1>
+					<span>Posted by <?php the_author(); ?><br><?php the_time(); echo(' '); the_date(); ?></span><br><br>
+					<?php the_content(); ?>
+				</article>
+			</div>
+		<?php endwhile; else:?>
+			<p>There are no posts or pages here</p>
+		<?php endif; ?>
+	</div>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 
-<p>This is the page.php file</p>
+	<div id='articlewrapper'>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class='fullspan article'>
+				<article class='container_12'>
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				</article>
+			</div>
+		<?php endwhile; else:?>
+			<p>There are no posts or pages here</p>
+		<?php endif; ?>
 
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
-
-<?php endwhile; else:?>
-
-	<p>There are no posts or pages here</p>
-
-<?php endif; ?>
+	</div>
 
 <?php get_footer(); ?>
