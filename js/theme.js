2f2401103e2758ec9f1eaf89e8b273262f5ae6d9
Sam <samjd1993@gmail.com>
2014-03-05 09:30:26 +1100

    Slow down interval speed of slider

--- js/theme.js
+++ js/theme.js
@@ -25,7 +25,7 @@ jQuery(document).ready(function($) {
 	$(function() {
 	    $('.slider').unslider({
 			speed: 500,               //  The speed to animate each slide (in milliseconds)
-			delay: 3000,              //  The delay between slide animations (in milliseconds)
+			delay: 5000,              //  The delay between slide animations (in milliseconds)
 			complete: function() {},  //  A function that gets called after every slide animation
 			keys: true,               //  Enable keyboard (left, right) arrow shortcuts
 			dots: true,               //  Display dot navigation
