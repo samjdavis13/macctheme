e8c671de5a7d36935e6e4f5d3e026f7124b2cf02
Sam <samjd1993@gmail.com>
2014-03-04 11:18:42 +1100

    Add links to single.php

--- front-page.php
+++ front-page.php
@@ -11,8 +11,9 @@
 		<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 			<div class='fullspan article'>
 				<article class='container_12'>
-					<h1><?php the_title(); ?></h1>
+					<a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
 					<?php the_field( 'content' ); ?>
+
 				</article>
 			</div>
 		<?php endwhile; else:?>
