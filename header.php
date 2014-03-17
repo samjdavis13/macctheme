4c923de6561b25aa37d3f093ee13842614f2522e
Sam <samjd1993@gmail.com>
2014-03-17 13:45:56 +1100

    Add font awesome

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 
 		</title>
 		<?php wp_head(); ?>
-
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 		<link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/favicon.png" />
 
 	</head>
